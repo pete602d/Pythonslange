b1a7469c3270a1d943cf0028b2ed7222ec95a10b
Esbern Holmes <holmes@ruc.dk>
2025-03-02 14:35:26 +0000

    updatet description

--- project_example/web_app.py
+++ project_example/web_app.py
@@ -1,10 +1,18 @@
+'''
+to run this server. 
+open codespace
+click termonal
+type cd project_example/ toi change to the esampel folder
+type pip install flask to install the flask web module
+type  python web_app.py to start the app
+'''
 from flask import Flask
 
 app = Flask(__name__)
 
 def generate_output():
     result = ""  # Create an empty string to store the output
-    for i in range(2, 10, 3):
+    for i in range(2, 13, 3):
         result += f"{i}\n"  # Append each number followed by a newline
     return result
 
